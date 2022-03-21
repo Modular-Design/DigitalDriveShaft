a021ac5ec032230c1659881bb230b02feceaac5a
Alrik-ILK <alrik.dargel@tu-dresden.de>
2022-03-21 08:38:42 +0100

    Korrektur
    kleine Fehler ausgebessert in strength

--- src/DigitalDriveShaft/analysis/strength.py
+++ src/DigitalDriveShaft/analysis/strength.py
@@ -1,4 +1,4 @@
-from ..cylindircal import DriveShaft
+from ..cylindrical import DriveShaft
 from ..basic import Loading
 import numpy as np
 
@@ -6,15 +6,15 @@ import numpy as np
 def calc_strength(shaft: DriveShaft, load: Loading):
     (shaft_radius, stackup) = shaft.get_value_in_iso_scale(0.5, 0.5)
     laminat_thickness = stackup.calc_thickness()
-    d_center_stackup = shaft_radius + (0.5 - shaft.get_contour_factor()) * laminat_thickness
+    d_center_stackup = 2 * shaft_radius + (0.5 - shaft.get_contour_factor()) * laminat_thickness
     u_shaft = np.pi * d_center_stackup
 
     nx = load.fx/u_shaft    #N/mm
-    ny = 0                  #N/mm   # TODO: @Alrik bitte Formel ergänzen
-    nxy = load.mx/d_center_stackup/2/u_shaft     #N/mm #ggf. Ergänzung von Fy und Fz komponenten
-    mx = 0                  #N      # TODO: @Alrik bitte Formel ergänzen
+    ny = 0                  #N/mm   
+    nxy = load.mx/d_center_stackup/2/u_shaft     #N/mm # TODO: @Alrik bitte Formel ergänzen von Fy und Fz komponenten
+    mx = 0                  #N      
     my = np.sqrt((load.my/1000/d_center_stackup/2)**2 + (load.mz/1000/d_center_stackup/2)**2)     #N
-    mxy = 0                         # TODO: @Alrik bitte Formel ergänzen
+    mxy = 0                         
 
     mech_load = np.array([nx, ny, nxy, mx, my, mxy])
     deformation = stackup.apply_load(mech_load)
