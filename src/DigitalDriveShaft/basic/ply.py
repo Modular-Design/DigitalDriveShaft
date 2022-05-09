610b7a56fc5bb44286fbdae29dc19bd95b64ab27
Willi Zschiebsch <willi.zschiebsch@gmail.com>
2022-05-09 19:16:21 +0200

    documentation

--- src/DigitalDriveShaft/basic/ply.py
+++ src/DigitalDriveShaft/basic/ply.py
@@ -1,18 +1,67 @@
 import numpy as np
-from .materials import Material, get_plane_stress_stiffness
+from .material import Material
 
 
 class Ply:
-    def __init__(self, material: Material, thickness: float, rotation=0.0):
+    def __init__(self,
+                 material: Material,
+                 thickness: float,
+                 rotation=0.0, degree=False):
+        """
+        Ply or Lamina
+
+        Parameters
+        ----------
+        material : Material
+            material of the ply
+        thickness : float
+            thickness of the ply
+        rotation : float, optional
+            rotation of the ply in [rad] or [deg] when degree is set True
+        degree : bool, optional
+            changes the measurement system of rotation, default is False
+
+        References
+        ----------
+        .. [1] J. Ashton and J.M. Whitney, "Theory of Laminated Plates",
+           Technomic, vol. 4, 1970
+        """
         self.material = material
         self.thickness = thickness
+        if degree:
+            rotation = rotation * np.pi / 180.0
         self.rotation = rotation
-        self.plane_stress = get_plane_stress_stiffness(self.material)
+        self.plane_stress = self.material.get_plane_stress_stiffness()
 
-    def rotate(self, rad) -> "Ply":
+    def rotate(self, rad, degree=False) -> "Ply":
+        """
+        Rotate Ply
+
+        Parameters
+        ----------
+        angle
+            angle in [rad] or [deg] when degree is set True
+        degree : bool, optional
+            changes the measurement system of angle, default is False
+
+        Returns
+        -------
+        Stackup
+            new instance of the rotated Stackup
+
+        Examples
+        --------
+
+        >>> ply = Ply(material, 1.0, 0.0)
+        >>> rot_ply = ply.rotate(90, degree=True)
+        Be aware, that the ply is left untouched.
+        To change this, use:
+        >>> ply = ply.rotate(90, degree=True)
+        instead.
+        """
         return Ply(self.material, self.thickness, self.rotation + rad)
 
-    def get_stiffness(self):
+    def get_stiffness(self) -> np.ndarray:
         m = np.cos(self.rotation)
         n = np.sin(self.rotation)
         T1 = np.matrix([[m ** 2, n ** 2, 2 * m * n],
@@ -25,22 +74,58 @@ class Ply:
         return stiffness_rot
 
     def get_material(self) -> Material:
+        """
+        Material of the ply
+
+        Returns
+        -------
+        Material
+            Material of the ply.
+        """
         return self.material
 
     def get_thickness(self) -> float:
+        """
+        Thickness of the ply
+
+        Returns
+        -------
+        float
+            Thickness of the ply.
+        """
         return self.thickness
 
-    def get_rotation(self) -> float:
+    def get_rotation(self, degree=False) -> float:
         """
-        returns Rotation in [RAD]
+        Rotation of the ply.
+
+        Parameters
+        ----------
+        degree : bool, optional
+            changes the measurement system of the return value, default is False
+
+        Returns
+        -------
+        float:
+            Rotation angle of the ply.
+            By default the Unit is in [rad], but can be [deg] if degree is set to True.
         """
+        if degree:
+            return self.rotation * 180.0 / np.pi
         return self.rotation
 
-    def get_local_stress(self, stress):
+    def get_local_stress(self, stress: np.ndarray) -> np.ndarray:
         """
+        Stress in ply coordinate system.
+        Parameters
+        ----------
+        stress : array
+            stress tensor in Voigt notation / [sig_11, sig_22, sig_12]
 
-        Returns:
-            object: 
+        Returns
+        -------
+        array
+            2d stress tensor in Voigt notation
         """
         angle = self.rotation  # TODO: Maybe -self.rotation
         m = np.cos(angle)
@@ -51,7 +136,19 @@ class Ply:
         stress_rot = T1_inv * stress
         return np.ravel(stress_rot)
 
-    def get_local_strain(self, strain):
+    def get_local_strain(self, strain: np.ndarray) -> np.ndarray:
+        """
+        Strain in ply coordinate system.
+        Parameters
+        ----------
+        strain : array
+            strain tensor in Voigt notation / [eps_11, eps_22, gamma_12]
+
+        Returns
+        -------
+        array
+            2d strain tensor in Voigt notation
+        """
         angle = self.rotation  # TODO: Maybe -self.rotation
         m = np.cos(angle)
         n = np.sin(angle)
--- src/DigitalDriveShaft/basic/stackup.py
+++ src/DigitalDriveShaft/basic/stackup.py
@@ -1,20 +1,55 @@
-from typing import List, Optional
+from typing import List, Tuple
 import numpy as np
 from .ply import Ply
-from .materials import TransverselyIsotropicMaterial, Material
+from .material import TransverselyIsotropicMaterial
 
 
 class Stackup:
     def __init__(self, plies: List[Ply]):
+        """
+        Stackup or Laminat
+
+        Parameters
+        ----------
+        plies : List[Ply]
+            list of lies that makes the stackup, order is bottom to top
+
+        References
+        ----------
+        .. [1] J. Ashton and J.M. Whitney, "Theory of Laminated Plates",
+           Technomic, vol. 4, 1970
+        """
         self.plies = plies
         self.thickness = self.calc_thickness()
         self.density = self.calc_density()
         self.abd = None
 
-    def get_plies(self):
+    def get_plies(self) -> List[Ply]:
+        """
+        Get plies of stackup.
+
+        Returns
+        -------
+        List[Ply]
+            lists of plies, order is bottom to top
+        """
         return self.plies
 
     def calc_thickness(self) -> float:
+        """
+        Calculate the stackup thickness
+
+        Notes
+        -----
+        **Calculation** means that that the result will allways computated again.
+        Meaning that it will always correct, but might take a little bit longer.
+        If you dont want this use **get_thickness()** instead!
+
+        Returns
+        -------
+        float
+            complete thickness of the stackup or sum over all ply thicknesses
+        """
         thickness = 0.0
         for ply in self.plies:
             thickness = thickness + ply.thickness
@@ -22,9 +57,37 @@ class Stackup:
         return self.thickness
 
     def get_thickness(self) -> float:
+        """
+        Returns the stackup thickness
+
+        Notes
+        -----
+        The Thickness is calculated at the beginning of the object creation.
+        Meaning that later changes will probably not be considered!
+        If you dont want this use **calc_thickness()** instead!
+
+        Returns
+        -------
+        float
+            complete thickness of the stackup or sum over all ply thicknesses
+        """
         return self.thickness
 
     def calc_density(self) -> float:
+        """
+        Calculate the stackup density
+
+        Notes
+        -----
+        **Calculation** means that that the result will allways computated again.
+        Meaning that it will always correct, but might take a little bit longer.
+        If you dont want this use **get_density()** instead!
+
+        Returns
+        -------
+        float
+            density of the stackup or mean over the plies
+        """
         thick_density = 0.0
         for i in range(len(self.plies)):
             ply = self.plies[i]
@@ -36,20 +99,71 @@ class Stackup:
         return self.density
 
     def get_density(self) -> float:
+        """
+        Returns the stackup density
+
+        Notes
+        -----
+        The Density is calculated at the beginning of the object creation.
+        Meaning that later changes will probably not be considered!
+        If you dont want this use **calc_density()** instead!
+
+        Returns
+        -------
+        float
+            density of the stackup or mean over the plies
+        """
         return self.density
 
-    def rotate(self, rad) -> "Stackup":
+    def rotate(self, angle, degree=False) -> "Stackup":
         """
-        returns Rotation in [RAD]
+        Rotate Stackup
+
+        Parameters
+        ----------
+        angle
+            angle in [rad] or [deg] when degree is set True
+        degree : bool, optional
+            changes the measurement system of angle, default is False
+
+        Returns
+        -------
+        Stackup
+            new instance of the rotated Stackup
+
+        Examples
+        --------
+
+        >>> stackup = Stackup([ply])
+        >>> rot_stackup = stackup.rotate(90, degree=True)
+        Be aware, that the stackup is left untouched.
+        To change this, use:
+        >>> stackup = stackup.rotate(90, degree=True)
+        instead.
+
         """
+        if degree:
+            angle = angle / 180.0 * np.pi
         plies = []
         for ply in self.plies:
-            plies.append(ply.rotate(rad))
+            plies.append(ply.rotate(angle))
         return Stackup(plies)
 
-    def get_abd(self, truncate=True):
+    def get_abd(self, truncate=True) -> np.ndarray:
+        """
+        Returns the ABD-Matrix of the stackup.
+
+        Parameters
+        ----------
+        truncate : bool, optional
+            when true: erase values smaller 1e-6. default: True
+
+        Returns
+        -------
+        array:
+            ABD-Matrix, dim=(6,6)
+        """
         if self.abd is None:
-            # Calculate the total thickness.
             h = self.thickness / 2
 
             # Create empty matricces for A B en D.
@@ -91,6 +205,14 @@ class Stackup:
         return self.abd
 
     def calc_homogenized(self) -> TransverselyIsotropicMaterial:
+        """
+        Homogenize the Stackup as a Transversely Isotropic Material.
+
+        Returns
+        -------
+        TransverselyIsotropicMaterial
+            Homogenized Stackup
+        """
         scale = 1.0 / self.get_thickness()
         e_1 = scale * (self.get_abd()[0, 0] - (self.get_abd()[0, 1]) ** 2 / self.get_abd()[1, 1])
         e_2 = scale * (self.get_abd()[1, 1] - (self.get_abd()[0, 1]) ** 2 / self.get_abd()[0, 0])
@@ -128,7 +250,26 @@ class Stackup:
         """
         return np.ravel(self.get_abd().dot(deformation))
 
-    def get_strains(self, deformation: np.ndarray):  # TODO: TEST it!
+    def get_strains(self, deformation: np.ndarray) -> List[Tuple[np.ndarray, np.ndarray]]:
+        """
+        Return strains in plies.
+
+        Parameters
+        ----------
+        deformation : array
+            deformation tensor of shape [e_11, e_11, e_12, x_11, x_22, x_12]
+            with e being the membrane strain, and x = curvature
+
+        Returns
+        -------
+        List[Tuple[np.ndarray, np.ndarray]]
+            list of strains (bottom to top) with tuple of the lower and upper strain
+
+        Notes
+        -----
+
+        Result can be used in get_stress(strains)
+        """
         # Calculating total thickness of the layup.
         h = self.thickness / 2
 
@@ -154,21 +295,37 @@ class Stackup:
             strain_lt_bot = ply.get_local_strain(strain_bot)
 
             # Store the strain values of this ply.
-            strain_ply = [strain_lt_bot, strain_lt_top]
+            strain_ply = (strain_lt_bot, strain_lt_top)
             strains.append(strain_ply)
             z_bot = z_top
 
         return strains
 
-    def get_stresses(self, strains):  # TODO: TEST it!
-        # Create a list for the stresses in each ply.
+    def get_stresses(self, strains: List[Tuple[np.ndarray, np.ndarray]]) -> List[Tuple[np.ndarray, np.ndarray]]:
+        """
+        Return stresses in plies.
+
+        Parameters
+        ----------
+        strains : list
+            list of strains in plies
+
+        Returns
+        -------
+        List[Tuple[np.ndarray, np.ndarray]]
+            list of stresses (bottom to top) with tuple of the lower and upper stress
+
+        Notes
+        -----
+
+        Result can be used in get_failure(stresses)
+        """
         stresses = []
 
         # Iterate over all plies.
         for i in range(len(self.plies)):
             # Obtain the strains from this ply.
-            strain_lt_bot = strains[i][0]
-            strain_lt_top = strains[i][1]
+            strain_lt_bot, strain_lt_top = strains[i]
 
             # Convert strains into stresses.
             Q = self.plies[i].get_stiffness()
@@ -176,16 +333,30 @@ class Stackup:
             stress_lt_bot = Q.dot(strain_lt_bot)
 
             # Store the stress values of this ply.
-            stress_ply = [stress_lt_bot, stress_lt_top]
+            stress_ply = (stress_lt_bot, stress_lt_top)
             stresses.append(stress_ply)
 
         return stresses
 
-    def get_failure(self, stresses):
+    def get_failure(self, stresses: List[Tuple[np.ndarray, np.ndarray]]) -> List[Tuple[dict, dict]]:
+        """
+        Return the failures in the plies.
+
+        Parameters
+        ----------
+        stresses : list
+            list of stresses in plies
+
+        Returns
+        -------
+        List[Tuple[dict, dict]]
+            list of failures (bottom to top) with tuple of the lower and upper failure
+
+        """
         failures = []
         for i in range(len(stresses)):
             ply_stress = stresses[i]
             material = self.plies[i].get_material()
-            failures.append([material.get_failure(stresses=ply_stress[0]),
-                             material.get_failure(stresses=ply_stress[1])])
+            failures.append((material.get_failure(stresses=ply_stress[0]),
+                             material.get_failure(stresses=ply_stress[1])))
         return failures
