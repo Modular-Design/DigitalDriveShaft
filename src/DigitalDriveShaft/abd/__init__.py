856035da0f210f1c3720b1f7cf02246021338967
Willi Zschiebsch <willi.zschiebsch@gmail.com>
2022-05-20 16:09:39 +0200

    clean up

--- src/DigitalDriveShaft/abd/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import deformation
-from . import abdcal
-from . import failure
-from . import homogenization
--- src/DigitalDriveShaft/abd/homogenization/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Calculate the ply properties from the properties of the constituents.
-A.J.J. Lagerweij
-COHMAS Mechanical Engineering KAUST
-2020
-"""
