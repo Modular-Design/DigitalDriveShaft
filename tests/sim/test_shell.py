ace9f33b9618d4c8974b7e52b7dc512e03e3ade2
Willi Zschiebsch <willi.zschiebsch@gmail.com>
2022-06-20 12:50:26 +0200

    added shell test

--- tests/sim/test_shell.py
+++ tests/sim/test_shell.py
@@ -6,29 +6,22 @@ from ansys.mapdl.core import launch_mapdl
 import numpy as np
 
 # default ElamX 2.6 Material
-material = TransverselyIsotropicMaterial(E_l=141000.0, E_t=9340.0,
+material = TransverselyIsotropicMaterial(E_l=141000.0, E_t=9340.0,  # N/mm^2
                                          nu_lt=0.35,
-                                         G_lt=4500.0, density=1.7E-9)
+                                         G_lt=4500.0, density=1.7E-6)  # kg/mm^3
+
+mapdl = launch_mapdl(mode="grpc", loglevel="ERROR")
 
 
-@pytest.mark.slow
-@pytest.mark.parametrize(
-    "laminat, stress",
-    [
-        ([Ply(material, 1, 0)], round(141000, 1)),
-        # ([Ply(material, 1, 90, degree=True)], round(1.0 / 9.34, 1))
-    ]
-)
-def test_shell(laminat, stress):
-    mapdl = launch_mapdl(mode="grpc", loglevel="ERROR")
+def shell_tests(laminat, stresses, significance):
+    # mapdl = launch_mapdl(mode="grpc", loglevel="ERROR")
     mapdl.finish()
     mapdl.clear()
     mapdl.verify()
-
     mapdl.prep7()
 
     """
-    
+
         2 - 3
     y   |   |
     ^   1 - 4
@@ -43,11 +36,12 @@ def test_shell(laminat, stress):
 
     elem_id = 1
     shell = Shell181(elem_id)
+    shell.set_layer_storage(1)  # store bot and top
 
     mat_id = 1
     for lam in laminat:
         material_to_mapdl(mapdl, lam.material, mat_id)
-        shell.add_layer(lam.thickness, mat_id, lam.get_rotation(degree=True))
+        shell.add_layer(lam.thickness, mat_id, lam.get_rotation(degree=True) + 90.0)
         mat_id += 1
     shell.add_to_mapdl(mapdl)
 
@@ -81,82 +75,41 @@ def test_shell(laminat, stress):
 
     mapdl.post1()
     mapdl.set(1)
-    # mapdl.post_processing.plot_nodal_eqv_stress()
-    mapdl.post_processing.plot_nodal_principal_stress('1',
-                                                      cpos='iso', window_size=[1024, 512])
 
-    mapdl.post_processing.plot_nodal_elastic_principal_strain('1',
-                                                              cpos='iso', window_size=[1024, 512])
-    result = mapdl.result
-    nodenump, stress = result.principal_nodal_stress(0)
+    for i in range(len(laminat)):
+        mapdl.layer(i + 1)
 
-    s1max = np.nanmax(stress[:, -5])
-    # s2max = np.nanmax(stress[:, -4])
+        sol_bot, sol_top = stresses[i]
+        sign_bot, sign_top = significance[i]
 
-    assert s1max == stress
-    mapdl.exit()
+        mapdl.shell('bot')
+        x_stresses = mapdl.post_processing.element_stress('X')  # nodal_component_stress('X')
+        sx_max = float(np.nanmax(x_stresses))
+        print(sx_max)
+        assert round(sx_max, sign_bot) == round(sol_bot, sign_bot)
+
+        mapdl.shell('top')
+        x_stresses = mapdl.post_processing.element_stress('X')
+        sx_max = float(np.nanmax(x_stresses))
+        print(sx_max)
+        assert round(sx_max, sign_top) == round(sol_top, sign_bot)
 
 
 @pytest.mark.slow
-def test_shell_stress():
-    mapdl = launch_mapdl(mode="grpc", loglevel="ERROR")
-    mapdl.finish()
-    mapdl.prep7()
+def test_shell():
+    # parameter = "laminat, stresses, significance",  # stresses list of layer stress bot -> top (bot, top)
+    params = [
+        ([Ply(material, 1, 0)], [(142153.51, 142153.51)], [(-3, -3)]),
+        ([Ply(material, 1, 0.1, degree=True)], [(142153.51, 142153.51)], [(-3, -3)]),
+        ([Ply(material, 1, 90, degree=True)], [(9416.41, 9416.41)], [(-3, -3)]),
+        ([Ply(material, 0.5, 0), Ply(material, 0.5, 0)],
+         [(142153.51, 142153.51), (142153.51, 142153.51)],
+         [(-3, -3), (-3, -3)]),
+        # ([Ply(material, 1, 90, degree=True), Ply(material, 1, 0)],[(9416.41, 9416.41), (142153.51, 142153.51)],
+        # [(-3, -3), (-3, -3)])
+    ]
 
-    p1 = mapdl.k(1, 0, 0, 0)
-    p2 = mapdl.k(2, 0, 1, 0)
-    p3 = mapdl.k(3, 1, 1, 0)
-    p4 = mapdl.k(4, 1, 0, 0)
-    mapdl.a(p1, p2, p3, p4)
+    for param in params:
+        shell_tests(*param)
 
-    # define material 1
-    mat_id1 = 1
-    mapdl.mp("EX", mat_id1, 1e5)
-    mapdl.mp("PRXY", mat_id1, 0.3)
-
-    # define material 2
-    mat_id2 = 2
-    mapdl.mp("EX", mat_id2, 2e5)
-    mapdl.mp("PRXY", mat_id2, 0.3)
-
-    elem_id = 1
-    mapdl.et(elem_id, "SHELL181")
-    mapdl.sectype(elem_id, type_="SHELL", name=f"shell181_{elem_id}")
-    mapdl.secdata(1, mat_id1, 0, 5)  # one layer of material 1 with 1 thickness
-    mapdl.secdata(1, mat_id2, 0, 5)  # one layer of material 2 with 1 thickness
-
-    # meshing
-    mapdl.asel("S", "AREA", '', elem_id)
-    mapdl.esize(0, 1)
-    mapdl.type(elem_id)
-    mapdl.secnum(elem_id)
-    mapdl.amesh("ALL")
-
-    mapdl.run("/solu")
-
-    # fixation
-    mapdl.nsel("S", "LOC", "X", 0)
-    mapdl.d("ALL", "UX", 0)
-    mapdl.d("ALL", "UY", 0)
-    mapdl.d("ALL", "UZ", 0)
-    mapdl.nsel("S", "LOC", "Y", 0)
-    mapdl.d("ALL", "ALL", 0)
-
-    # loading
-    mapdl.nsel("S", "LOC", "X", 1)
-    mapdl.d("ALL", "UX", 1)  # strain bc
-
-    # select everything before solving
-    mapdl.allsel()
-    mapdl.antype("STATIC")
-    mapdl.outres("ALL", "ALL")
-    mapdl.solve()
-    mapdl.finish()
-    mapdl.post1()
-    mapdl.set(1)
-
-    result = mapdl.result
-
-    # this does not work
-    nodenump, stress = result.principal_nodal_stress(0)
-    s1max = np.nanmax(stress[:, -5])
+    mapdl.exit()
