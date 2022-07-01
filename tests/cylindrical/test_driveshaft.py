8873f47d7552b9b0700b890a668ac1577f7427ac
Willi Zschiebsch <willi.zschiebsch@gmail.com>
2022-07-01 14:27:41 +0200

    add more driveshaft testing

--- tests/cylindrical/test_driveshaft.py
+++ tests/cylindrical/test_driveshaft.py
@@ -70,38 +70,43 @@ def test_get_crossection(form, stackup, pos_z, crossection, err):
 
 
 @pytest.mark.parametrize(
-    "form, stackup, pos_z, area_mass, err",
+    "form, stackup, pos_z, iso, area_mass, err",
     [
         (
                 CylindricalForm(lambda z, phi: 10, 20),
                 CylindricalStackup(lambda z, phi: Stackup([Ply(dens1_mat, 1)])),
-                0, 65.9734, 1e-2
+                0, True, 65.9734, 1e-2
         ),  # int int r dr dphi phi=0..2pi r=10..11
         (
                 CylindricalForm(lambda z, phi: 10, 20),
                 CylindricalStackup(lambda z, phi: Stackup([Ply(dens1_mat, 3)])),
-                0, 216.77, 1e-2
+                0, True, 216.77, 1e-2
         ),  # int int 11 r dr dphi phi=0..2pi r=10..13
         (
                 CylindricalForm(lambda z, phi: 5, 1),
                 CylindricalStackup(lambda z, phi: Stackup([Ply(dens1_mat, 5)])),
-                0, 235.619, 1e-2
+                0, True, 235.619, 1e-2
         ),  # int int r dr dphi phi=0..2pi r=5..10
         (
                 CylindricalForm(lambda z, phi: 10, 20),
                 CylindricalStackup(lambda z, phi: Stackup([Ply(dens10_mat, 1)])),
-                0, 659.734, 1e-2
+                0, True, 659.734, 1e-2
         ),  # int int 10 r dr dphi phi=0..2pi r=10..11
         (
                 CylindricalForm(lambda z, phi: 5, 20),
                 CylindricalStackup(lambda z, phi: Stackup([Ply(dens10_mat, 1), Ply(dens1_mat, 3)])),
-                1.0, 486.947, 1e-2
-        )  # int int 10 r dr dphi phi=0..2pi r=5..6 + int int 1 r dr dphi phi=0..2pi r=6..9
+                1.0, True, 486.947, 1e-2
+        ),  # int int 10 r dr dphi phi=0..2pi r=5..6 + int int 1 r dr dphi phi=0..2pi r=6..9
+        (
+                CylindricalForm(lambda z, phi: 10, 10, 0),
+                CylindricalStackup(lambda z, phi: Stackup([Ply(mix_mats_on_z(z, 10), 1)]), 10, 0),
+                2.0, False, 65.9734, 1e-1
+        ),  # int int 1 * r dr dphi phi=0..2pi r=10..11
     ]
 )
-def test_get_area_mass(form, stackup, pos_z, area_mass, err):
+def test_get_area_mass(form, stackup, pos_z, iso, area_mass, err):
     shaft = DriveShaft(form, stackup, 0)
-    result = shaft.get_area_mass(pos_z)
+    result = shaft.get_area_mass(pos_z, iso)
     assert np.fabs(result - area_mass) <= err
 
 
@@ -145,10 +150,20 @@ def test_get_volume(form, stackup, volume, err):
                 13194.7, 1e-1
         ),  # int int int 10 * r dr dphi dz phi=0..2pi r=10..11 z=0..20
         (
-                CylindricalForm(lambda z, phi: 10, 20),
-                CylindricalStackup(lambda z, phi: Stackup([Ply(mix_mats_on_z(z, 0.5), 1)])),
-                7257.08, 1e-1
-        )  # {int int int 1 * r dr dphi dz phi=0..2pi r=10..11 z=0..10} + {int int int 10 * r dr dphi dz phi=0..2pi r=10..11 z=10..20}
+                CylindricalForm(lambda z, phi: 10, 10, 0),
+                CylindricalStackup(lambda z, phi: Stackup([Ply(mix_mats_on_z(z, 10), 1)]), 10, 0),
+                659.734, 1e-1
+        ),  # {int int int 1 * r dr dphi dz phi=0..2pi r=10..11 z=0..10}
+        (
+                CylindricalForm(lambda z, phi: 10, 20, 10),
+                CylindricalStackup(lambda z, phi: Stackup([Ply(mix_mats_on_z(z, 9), 1)]), 20, 10),
+                6597.34, 1e-1
+        ),  # {int int int 10 * r dr dphi dz phi=0..2pi r=10..11 z=10..20}
+        # (
+        #         CylindricalForm(lambda z, phi: 10, 20),
+        #         CylindricalStackup(lambda z, phi: Stackup([Ply(mix_mats_on_z(z, 10), 1)]), 20),
+        #         7257.08, 1e-1
+        # ) # {int int int 1 * r dr dphi dz phi=0..2pi r=10..11 z=0..10} + {int int int 10 * r dr dphi dz phi=0..2pi r=10..11 z=10..20}
     ]
 )
 def test_get_mass(form, stackup, density, err):
@@ -172,10 +187,15 @@ def test_get_mass(form, stackup, density, err):
                 10.0, 1e-2
         ),
         (
-                CylindricalForm(lambda z, phi: 10, 20),
-                CylindricalStackup(lambda z, phi: Stackup([Ply(mix_mats_on_z(z, 0.5), 1)])),
-                6.5, 1e-2
-        )
+                CylindricalForm(lambda z, phi: 5, 20),
+                CylindricalStackup(lambda z, phi: Stackup([Ply(dens10_mat, 1), Ply(dens1_mat, 3)])),
+                2.76786, 1e-2
+        ),  # (int int 10 r dr dphi phi=0..2pi r=5..6 + int int 1 r dr dphi phi=0..2pi r=6..9) / (int int r dr dphi phi=0..2pi r=5..9)
+        # (
+        #         CylindricalForm(lambda z, phi: 10, 20),
+        #         CylindricalStackup(lambda z, phi: Stackup([Ply(mix_mats_on_z(z, 0.5), 1)])),
+        #         6.5, 1e-2
+        # )
     ]
 )
 def test_get_density(form, stackup, density, err):
